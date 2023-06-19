2477f6ac3f92fb5e0d42db0ecd0cdaf8fe862cc0
khanh <emma.devn@gmail.com>
2023-06-19 14:00:54 +0700

    commit input and str methods

---
+++ input/input_ex.py
@@ -0,0 +1,21 @@
+"""
+type from input is always default string
+"""
+# name = input("input your name: ")
+# print(f"hello {name}")
+# age = int(input("input your age: "))
+# print(f"your age is {age}")
+
+# exercises
+# adj = input("Enter an adj: ")
+# number = int(input("Enter a number: "))
+# print(f"today I went to a {adj} zoo.")
+# print(f"In the zoo, I saw {number} young elephants.")
+
+# width = float(input("Please input width: "))
+# length = float(input("Please input length: "))
+# print(f"the area is {width * length}")
+
+# quantity = int(input("How many items you want to buy? "))
+# price = float(input("What is the price of each item? "))
+# print(f"the total of your order is: {round(quantity * price, 3)}")
---
+++ str_methods/str_methods.py
@@ -0,0 +1,36 @@
+"""
+Learn some useful str methods
+"""
+
+# name = input("Enter your name: ")
+# result = len(name)  # length includes space
+# print(result)
+# print(name.find(" "))  # return position of the first occurence
+# print(name.rfind("n"))
+# print(name.rfind("x"))  # return -1 if unfound
+# print(name.capitalize())
+# print(name.upper())
+# print(name.lower())
+# print(name.isdigit())
+# print(name.isalpha())
+
+
+# phone_num = input("your phone: ")
+# print(phone_num.count("-"))
+# print(phone_num.replace("-", " "))
+
+"""
+Exercise
+username is no more than 12 chars
+username must not contain space
+username must not contain digits
+"""
+# username = input("Your name: ")
+# if len(username) > 12:
+#     print(f"{username} can't be more than 12 chars")
+# elif username.find(" ") != -1:
+#     print(f"{username} should not contain space")
+# elif not username.isalpha():
+#     print(f"{username} should not contain digits")
+# else:
+#     print(f"Welcome {username.capitalize()}")
